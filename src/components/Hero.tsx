409a9be821b8452dae8fae53ccaf39c788f22e2b
GabrielCee27 <gabrielcee81@gmail.com>
2023-04-13 09:34:26 -0500

    responsive hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,36 +22,40 @@ const ICON_WIDTH = 20;
 
 const Hero = () => {
   return (
-    <section className="min-h-screen">
-      <div className="flex flex-col gap-4">
-        <h1
-          className={`text-[.6rem] ${inter.className} text-gray-300 tracking-[.5em] above-title`}
-        >
-          MY NAME IS
-        </h1>
-        <h2 className="font-semibold text-4xl">
-          Gabriel <span className="text-blue-300">Cisneros.</span>
-        </h2>
-        <p className={`${inter.className} text-sm text-gray-200`}>
-          Full Stack Software Engineer with 4+ years of experience with an
-          emphasis in React. I am currently the team lead of Seller Retention at
-          The RealReal which involves working in a fast-pace agile environment
-          and ensuring seller facing features are delivered on time. Having a
-          preference for frontend, I am excited about any opportunity involving
-          customer facing React applications.
-        </p>
-        <div className="flex flex-row gap-5">
-          {EXTERNAL_LINKS.map(({ href, icon, alt }) => (
-            <a href={href} target="_blank">
-              <Image
-                src={icon}
-                alt={alt}
-                width={ICON_WIDTH}
-                height={ICON_WIDTH}
-              />
-            </a>
-          ))}
+    <section className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col-reverse justify-end gap-4 md:flex-row md:justify-around md:gap-0 items-center w-full">
+        <div className="flex flex-col gap-5 justify-center max-w-md">
+          <h1
+            className={`text-[.6rem] ${inter.className} text-gray-300 tracking-[.5em] above-title`}
+          >
+            MY NAME IS
+          </h1>
+          <h2 className="font-semibold text-4xl">
+            Gabriel <span className="text-blue-300">Cisneros.</span>
+          </h2>
+          <p className={`${inter.className} text-sm text-gray-200`}>
+            Full Stack Software Engineer with 4+ years of experience with an
+            emphasis in React. I am currently the team lead of Seller Retention
+            at The RealReal which involves working in a fast-pace agile
+            environment and ensuring seller facing features are delivered on
+            time. Having a preference for frontend, I am excited about any
+            opportunity involving customer facing React applications.
+          </p>
+          <div className="flex flex-row gap-5">
+            {EXTERNAL_LINKS.map(({ href, icon, alt }) => (
+              <a href={href} target="_blank">
+                <Image
+                  src={icon}
+                  alt={alt}
+                  width={ICON_WIDTH}
+                  height={ICON_WIDTH}
+                />
+              </a>
+            ))}
+          </div>
         </div>
+
+        <div className="w-[125px] h-[150px] md:w-[340px] md:h-[398px] bg-blue-200 rounded-md" />
       </div>
     </section>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <Navigation />
       </header>
 
-      <main className="w-full min-h-screen px-10 mt-[72px]">
+      <main className="w-full min-h-screen px-10">
         <Hero />
       </main>
     </>
