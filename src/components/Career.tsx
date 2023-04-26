06564286a01100c98822273ac90f10c7d4bafed6
GabrielCee27 <gabrielcee81@gmail.com>
2023-04-25 21:50:40 -0500

    career section content

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -2,20 +2,93 @@ import { FC, useState } from "react";
 import Title from "./Text/Title";
 import Pills from "./Pills";
 
-const WORK_EXPERIENCE = [
+const DescItems: FC<{ items: React.ReactNode[] }> = ({ items }) => {
+  return (
+    <ul className="m-0 p-0 grid gap-2 md:gap-1">
+      {items.map((item, index) => (
+        <li className="grid grid-cols-[auto_1fr] gap-1" key={index}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+interface Experience {
+  company: string;
+  role: string;
+  location: string;
+  dates: string;
+  description: string[] | React.ReactNode;
+  technology?: string[];
+}
+
+const WORK_EXPERIENCE: Experience[] = [
   {
     company: "The RealReal",
     role: "Senior Full Stack Engineer",
     location: "Remote",
     dates: "Nov 2020 - Current",
+    description: (
+      <DescItems
+        items={[
+          "Lead the migration of our seller dashboard, My Sales, to a Next.js app using Typescript and Apollo Client",
+          "Create backend solutions for Rails and Phoenix applications which include maintaining Stripe webhooks, REST endpoints for consignment information, and creating Plaid Link GraphQL mutations",
+          "Lead the adoption of Next’s Incremental Static Regeneration functionality for Prismic (CMS) pages which improved Lighthouse Performance scores by 7 points on average",
+          "Help team growth by conducting interviews, mentoring an intern, and being active in our JAMstack Guild",
+        ]}
+      />
+    ),
+    technology: ["Next.js", "TypeScript", "GraphQL", "Elixir", "Ruby on Rails"],
   },
   {
     company: "H.O.M.E",
     role: "Freelance Full Stack Engineer",
     location: "Remote",
     dates: "April 2020 - Nov 2020",
+    description: (
+      <DescItems
+        items={[
+          "Designed and developed a video course web application with Django, React, PostgreSQL, and AWS S3",
+          "Communicated with client on a weekly basis to review progress and create achievable deliverables",
+          "Implemented Redux-like state with React’s Context and Reducer hooks to keep video angles in sync",
+        ]}
+      />
+    ),
+    technology: ["React", "AWS", "Django", "Python"],
   },
-] as const;
+  {
+    company: "Conventus",
+    role: "Full Stack Developer",
+    location: "San Francisco, CA",
+    dates: "May 2019 - June 2020",
+    description: (
+      <DescItems
+        items={[
+          "Part of a team which developed an internal loan management application with Django and React",
+          "Created a socket.io, Node micro-service to enable real-time loan editing which helped avoid hours of work being lost due to employees overwriting each others changes",
+          "Implemented a price forecast chart powered by HouseCanary and Zillow APIs",
+        ]}
+      />
+    ),
+    technology: ["Django", "React", "Express", "Socket.IO", "Python"],
+  },
+  {
+    company: "JAKKS Pacific",
+    role: "Full Stack Developer Intern",
+    location: "Santa Monica, CA",
+    dates: "May 2018 - Aug 2018",
+    description: (
+      <>
+        Participated in improving the company&#39;s intranet php website used by
+        over 700 employees by being engaged in scrum meetings, creating
+        solutions upon ticket submission, and executing automated Selenium
+        frontend tests.
+      </>
+    ),
+    technology: ["Flask", "Python", "PHP"],
+  },
+];
 
 const Career: FC = () => {
   const [selectedTab, setSelectedTab] = useState(0);
@@ -24,8 +97,11 @@ const Career: FC = () => {
     <div className="flex flex-col gap-5">
       <Title aboveTitle="CAREER PATH">Work Experience</Title>
 
-      <div className="flex flex-col gap-4 md:gap-24 md:flex-row">
-        <ul className="flex flex-row gap-4 md:flex-col md:w-[40%] md:justify-center">
+      <div className="flex flex-col gap-3 md:gap-24 md:flex-row pt-4">
+        <ul
+          id="career-tabs"
+          className="pb-1 md:pb-0 overflow-x-auto flex flex-row gap-1 md:gap-4 md:flex-col md:w-[40%] md:justify-start md:overflow-x-auto"
+        >
           {WORK_EXPERIENCE.map((ex, index) => {
             const isSelected = selectedTab == index;
             return (
@@ -33,7 +109,7 @@ const Career: FC = () => {
                 <button
                   className={`${
                     isSelected ? "bg-blue-300" : ""
-                  } rounded-full py-2 px-3 md:w-[90%] md:text-left md:rounded-md md:py-3 md:px-4`}
+                  } rounded-full py-2 px-3 md:w-[90%] md:text-left md:rounded-md md:py-3 md:px-4 whitespace-nowrap`}
                   onClick={() => {
                     setSelectedTab(index);
                   }}
@@ -49,12 +125,14 @@ const Career: FC = () => {
           })}
         </ul>
 
-        <div className="flex flex-col gap-4 md:w-[60%]">
+        <div className="md:w-[60%] md:h-96">
           {WORK_EXPERIENCE.map((ex, index) => {
             const isSelected = selectedTab == index;
             return (
               <div
-                className={`${isSelected ? "block" : "hidden"}`}
+                className={`${
+                  isSelected ? "block" : "hidden"
+                } flex flex-col gap-4`}
                 id={`career-tabpanel-${index}`}
                 role="tabpanel"
                 aria-labelledby={`career-tab-${index}`}
@@ -65,20 +143,17 @@ const Career: FC = () => {
                   <p className="text-xs text-gray-300 font-medium">
                     {ex.dates}
                   </p>
-                  <Pills items={["Next.js", "Prismic"]} />
+                  {ex.technology?.length ? (
+                    <Pills items={ex.technology} />
+                  ) : null}
+                </div>
+                <div className="h-[.5px] w-full bg-gray-300/50" />
+                <div className="career-body text-sm text-gray-200">
+                  {ex.description}
                 </div>
               </div>
             );
           })}
-
-          <div className="h-[.5px] w-full bg-gray-300/50" />
-          <div className="career-body text-sm text-gray-200">
-            <ul>
-              {["Improve Site", "Improve Site"].map((item) => (
-                <li className="pt-1">{item}</li>
-              ))}
-            </ul>
-          </div>
         </div>
       </div>
     </div>
--- src/components/Pills.tsx
+++ src/components/Pills.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Pills: FC<{ items: React.ReactNode[] }> = ({ items }) => {
   return (
-    <ul className="flex flex-row gap-2">
+    <ul className="flex flex-row gap-2 flex-wrap">
       {items.map((item) => (
         <li className="rounded py-0.5 px-1.5 border border-gray-300 text-xs">
           {item}
