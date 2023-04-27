d2a0d4b9b8534a3830f9546d7f4c56c738572b52
GabrielCee27 <gabrielcee81@gmail.com>
2023-04-27 09:16:41 -0500

    demo and code links

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,29 +1,26 @@
 import Title from "./Text/Title";
 import NextImage from "next/image";
 import Pills from "./Pills";
-import { LinkIcon } from "@heroicons/react/24/solid";
+import { CodeBracketIcon, LinkIcon } from "@heroicons/react/20/solid";
 
 interface Project {
   name: string;
   image: string;
-  href: string;
+  githubLink?: string;
+  demoLink?: string;
   tech?: string[];
 }
 
 const PROJECTS: Project[] = [
   {
-    name: "EEG Capstone",
-    image: "/images/projects/jesse-orrico-60373-unsplash.jpeg",
-    tech: ["Flask", "Python"],
-    href: "www.google.com",
-  },
-  {
     name: "Portfolio Site",
     image: "/images/projects/jesse-orrico-60373-unsplash.jpeg",
     tech: ["Next.js", "Tailwind"],
-    href: "https://github.com/GabrielCee27/portfolio",
+    githubLink: "https://github.com/GabrielCee27/portfolio",
   },
 ];
+
+const ICON_CLASSNAME = "fill-gray-600 w-5 h-5 hover:fill-black rounded-full";
 
 const Projects = () => {
   return (
@@ -31,33 +28,44 @@ const Projects = () => {
       <Title aboveTitle="MY WORK">Projects</Title>
       <ul className="grid grid-cols-[repeat(auto-fill,_minmax(256px,_1fr))] w-full py-10 gap-5">
         {PROJECTS.map((p, index) => {
+          const link = p.demoLink || p.githubLink;
           return (
             <li
               className="m-auto w-full h-64 relative flex flex-col justify-end cursor-pointer"
               key={index}
+              onClick={() => {
+                link && window.open(link, "_blank");
+              }}
             >
-              <a href={p.href} target="_blank">
-                <NextImage
-                  fill
-                  style={{ objectFit: "cover" }}
-                  src={p.image}
-                  alt="Placeholder"
-                  className="rounded"
-                />
-                <div className="relative rounded-b bg-slate-100 py-4 px-3 text-black flex flex-col gap-1">
-                  <p>{p.name}</p>
-                  <div className="flex flex-row justify-between">
-                    {p.tech?.length ? (
-                      <span className="text-gray-700">
-                        <Pills items={p.tech} />
-                      </span>
+              <NextImage
+                fill
+                style={{ objectFit: "cover" }}
+                src={p.image}
+                alt="Placeholder"
+                className="rounded"
+              />
+              <div className="relative rounded-b bg-slate-100 py-4 px-3 text-black flex flex-col gap-1">
+                <p>{p.name}</p>
+                <div className="flex flex-row justify-between">
+                  {p.tech?.length ? (
+                    <span className="text-gray-700">
+                      <Pills items={p.tech} />
+                    </span>
+                  ) : null}
+                  <div className="flex flex-row gap-3">
+                    {p.demoLink ? (
+                      <a href={p.demoLink} target="_blank">
+                        <LinkIcon className={ICON_CLASSNAME} />
+                      </a>
                     ) : null}
-                    {p.href ? (
-                      <LinkIcon className="h-6 w-6 text-black" />
+                    {p.githubLink ? (
+                      <a href={p.githubLink} target="_blank">
+                        <CodeBracketIcon className={ICON_CLASSNAME} />
+                      </a>
                     ) : null}
                   </div>
                 </div>
-              </a>
+              </div>
             </li>
           );
         })}
