a6f019d7778556c192981838aa6da60ba406012b
GabrielCee27 <gabrielcee81@gmail.com>
2026-03-14 19:03:52 -0700

    add profile image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,18 @@
 import { Inter } from "next/font/google";
 import NextImage from "next/image";
+import Avatar from "./public/images/avatar.png";
 import Title from "./Text/Title";
 import ExternalIconLinks from "./ExternalIconLinks";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Hero = () => {
+
+  const avatarCard = (
+    <div className="flex items-end h-72 w-72 md:w-96 relative">
+      <NextImage src="/images/profile_image.jpeg" alt="avatar" loading="eager" fill={true} style={{ objectFit: "cover" }} className="rounded" />
+    </div>
+  );
 
   return (
     <section
@@ -18,15 +25,17 @@ const Hero = () => {
             Gabriel <span className="text-blue-300">Cisneros.</span>
           </Title>
           <p className={`${inter.className} text-sm text-gray-200`}>
-          Software Engineer with 4+ years of experience creating web applications with cutting-edge engineering solutions for internal and external facing applications.
-           Proven ability to lead  projects to production. Extensive expertise in React as well as micro-services.
+            Software Engineer with 4+ years of experience creating web applications with cutting-edge engineering solutions for internal and external facing applications.
+            Proven ability to lead  projects to production. Extensive expertise in React as well as micro-services.
           </p>
           <div className="flex flex-row gap-5">
             <ExternalIconLinks />
           </div>
         </div>
+        <div className="hidden md:block">{avatarCard}</div>
       </div>
-    </section>
+      <div className="block md:hidden">{avatarCard}</div>
+    </section >
   );
 };
 
